87bf28185b3c635d06a36678b1d6b2ec281cf83d
DanielDaye <danieljdaye@gmail.com>
2024-10-27 15:58:12 -0400

    Updated to include combo histogram

--- code/organ_weight_EDA.R
+++ code/organ_weight_EDA.R
@@ -11,20 +11,25 @@ ggplot(lemon, aes(length, body)) +
   geom_point() + 
   labs(title = "Lemon shark", x = "TL (cm)", y = "Body Weight (kg)") +
   theme_bw()
+ggsave("figures/EDA/lemon_TL_BW.png")
 # Heart
 ggplot(lemon, aes(length, heart)) +
   geom_point() + 
   labs(title = "Lemon shark", x = "TL (cm)", y = "Heart Weight (kg)") +
   theme_bw()
+ggsave("figures/EDA/lemon_TL_heart.png")
 # Liver
 ggplot(lemon, aes(length, liver)) +
   geom_point() + 
   labs(title = "Lemon shark", x = "TL (cm)", y = "Liver Weight (kg)") +
   theme_bw()
+ggsave("figures/EDA/lemon_TL_liver.png")
 # Length Hist
 ggplot(lemon, aes(length)) +
   geom_histogram() + 
-  labs(title = "Lemon shark", x = "TL")
+  labs(title = "Lemon shark", x = "TL", y = "Count") +
+  theme_bw()
+ggsave("figures/EDA/lemon_TL_hist.png")
 
 
 # Makos ----
@@ -33,21 +38,37 @@ ggplot(makos, aes(length, body)) +
   geom_point() + 
   labs(title = "Mako shark", x = "TL (cm)", y = "Body Weight (kg)") +
   theme_bw()
+ggsave("figures/EDA/makos_TL_BW.png")
 # Heart
 ggplot(makos, aes(length, heart)) +
   geom_point() + 
   labs(title = "Mako shark", x = "TL (cm)", y = "Heart Weight (kg)") +
   theme_bw()
+ggsave("figures/EDA/makos_TL_heart.png")
 # Liver
 ggplot(makos, aes(length, liver)) +
   geom_point() + 
   labs(title = "Mako shark", x = "TL (cm)", y = "Liver Weight (kg)") +
   theme_bw()
+ggsave("figures/EDA/makos_TL_liver.png")
 # Length Hist
 ggplot(makos, aes(length)) +
   geom_histogram() + 
-  labs(title = "Mako shark", x = "TL")
+  labs(title = "Mako shark", x = "TL", y = "Count") +
+  theme_bw()
+ggsave("figures/EDA/makos_TL_hist.png")
 
+
+# Combined Histogram
+combo <- data.frame(tl = c(lemon$length, makos$length),
+                    Species = c(rep("Lemon",nrow(lemon)),
+                                rep("Mako", nrow(makos))))
+ggplot(combo, aes(tl, fill = Species)) +
+  geom_histogram() +
+  coord_cartesian(xlim = c(25,300), ylim = c(0, 40)) +
+  labs(x = "Total Length (cm)", y = "Count") +
+  theme_bw()
+ggsave("figures/EDA/combo_hist.png")
 
 # Pairs.Panels
 pp.lemon <- lemon %>%
